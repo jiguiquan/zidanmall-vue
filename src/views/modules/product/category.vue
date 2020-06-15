6568a43dc04aecf2526fb1f50a5ca5114960bfd0
jiguiquan@163.com <An831121>
2020-06-15 21:54:56 +0800

    增加三级分类树的edit修改功能

--- src/views/modules/product/category.vue
+++ src/views/modules/product/category.vue
@@ -7,6 +7,7 @@
       show-checkbox
       node-key="catId"
       :default-expanded-keys="expandedKey"
+      draggable
     >
       <span class="custom-tree-node" slot-scope="{ node, data }">
         <span>{{ node.label }}</span>
@@ -17,6 +18,7 @@
             size="mini"
             @click="() => append(data)"
           >Append</el-button>
+          <el-button type="text" size="mini" @click="() => edit(data)">Edit</el-button>
           <el-button
             v-if="node.childNodes.length == 0"
             type="text"
@@ -26,15 +28,26 @@
         </span>
       </span>
     </el-tree>
-    <el-dialog title="提示" :visible.sync="dialogVisible" width="30%">
+    <el-dialog
+      :title="title"
+      :visible.sync="dialogVisible"
+      width="30%"
+      :close-on-click-modal="false"
+    >
       <el-form :model="category">
         <el-form-item label="分类名称">
           <el-input v-model="category.name" autocomplete="off"></el-input>
         </el-form-item>
+        <el-form-item label="图标">
+          <el-input v-model="category.icon" autocomplete="off"></el-input>
+        </el-form-item>
+        <el-form-item label="计量单位">
+          <el-input v-model="category.productUnit" autocomplete="off"></el-input>
+        </el-form-item>
       </el-form>
       <span slot="footer" class="dialog-footer">
         <el-button @click="dialogVisible = false">取 消</el-button>
-        <el-button type="primary" @click="addCategory">确 定</el-button>
+        <el-button type="primary" @click="submitData">确 定</el-button>
       </span>
     </el-dialog>
   </div>
@@ -45,12 +58,17 @@ export default {
   components: {},
   data() {
     return {
+      dialogType: "", //edit/add
+      title: "",
       category: {
         name: "",
         parentCid: 0,
         catLevel: 0,
         showStatus: 1,
-        sort: 0
+        sort: 0,
+        icon: "",
+        productUnit: "",
+        catId: null
       },
       menus: [],
       dialogVisible: false,
@@ -72,9 +90,16 @@ export default {
     },
     append(data) {
       console.log("append", data);
-      this.dialogVisible = true;
+      this.dialogType = "add";
+      (this.title = "添加分类"), (this.dialogVisible = true);
       this.category.parentCid = data.catId;
       this.category.catLevel = data.catLevel * 1 + 1;
+      this.category.catId = null;
+      this.category.name = "";
+      this.category.icon = "";
+      this.category.productUnit = "";
+      this.category.sort = 0;
+      this.category.showStatus = 1;
     },
     //对话框中用的添加分类的方法
     addCategory() {
@@ -90,7 +115,55 @@ export default {
         });
         this.dialogVisible = false;
         this.getMenus();
-        this.expandedKey = [this.category.parentCid]
+        this.expandedKey = [this.category.parentCid];
+      });
+    },
+
+    edit(data) {
+      console.log("要修改的数据", data);
+      this.dialogType = "edit";
+      (this.title = "修改分类"), (this.dialogVisible = true);
+
+      //发送请求获取当前节点最新的数据
+      this.$http({
+        url: this.$http.adornUrl(`/product/category/info/${data.catId}`),
+        method: "get"
+      }).then(({ data }) => {
+        console.log("要回显的数据", data);
+        this.category.name = data.data.name;
+        this.category.catId = data.data.catId;
+        this.category.icon = data.data.icon;
+        this.category.productUnit = data.data.productUnit;
+        this.category.parentCid = data.data.parentCid;
+      });
+    },
+
+    submitData() {
+      if (this.dialogType == "add") {
+        console.log("add");
+        this.addCategory();
+      } else if (this.dialogType == "edit") {
+        console.log("edit");
+        this.editCategory();
+      }
+    },
+
+    editCategory() {
+      var { catId, name, icon, productUnit } = this.category; //解构表达式
+      this.$http({
+        url: this.$http.adornUrl("/product/category/update"),
+        method: "post",
+        data: this.$http.adornData({ catId, name, icon, productUnit }, false)
+      }).then(({ data }) => {
+        this.$message({
+          message: "分类修改成功",
+          type: "success"
+        });
+        this.dialogVisible = false;
+        //刷新新分类树
+        this.getMenus();
+        //设置需要被展开的树节点，当前节点的父节点
+        this.expandedKey = [this.category.parentCid];
       });
     },
 
